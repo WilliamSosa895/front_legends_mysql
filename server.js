fc6193f5f1347f14aae318fe3ea1704ec3941f0f
WilliamSosa895 <zs23004708@estudiantes.uv.mx>
2025-05-01 14:45:09 -0600

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '.')));
 
 // Siempre responde index.html para rutas desconocidas (SPA)
-app.get('/:splat(*)', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
